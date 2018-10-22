31962fd4bfb6aa386fbcef07bd5753ef70caca00
Neil McCoy <neil.mccoy@outlook.com>
2018-10-22 22:13:08 +1100

    Functions in rust

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 fn main() {
-    let (x, y) = (1,5);
+    let (x, y) = assign_numbers();
 
+    print_two_numbers(x, y);
+}
+   
+fn assign_numbers() -> (i32, i32){
+    (1, 5)
+}
+
+fn print_two_numbers(x: i32, y: i32){
     println!("x:{}, y:{}",x,y);
 }
